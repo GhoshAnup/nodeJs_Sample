58b3d32a58d5feb6c6a058eb4d32779aef49d491
GhoshAnup <np.ghsh@gmail.com>
2017-11-13 15:08:39 +0530

    Add project files.

---
+++ DemoNodeApp/server.js
@@ -0,0 +1,27 @@
+'use strict';
+//var http = require('http');
+//var port = process.env.PORT || 1337;
+
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('Hello World\n');
+//}).listen(port);
+
+var http = require('http');
+var express = require("express");
+var app = express();
+
+// Setup the View Engine
+app.set("view engine", "jade");
+
+app.get("/", function (req, res) {
+    res.render("jade/index", {title:"Express+Jade"})
+});
+
+app.get("/api/users", function (req, res) {
+    res.set("Content-Type", "application/json");
+    res.send({ name: "Anup", isValid: true, group: "Admin" });
+});
+
+var server = http.createServer(app);
+server.listen(3000);
