37b882d7e30c4da5d02944cbc243816a32c0333d
GhoshAnup <np.ghsh@gmail.com>
2017-11-16 19:55:08 +0530

    add file

---
+++ DemoNodeApp/data/setupMongoDbjs.js
@@ -0,0 +1,18 @@
+﻿
+//Setup Mongo Db server....
+
+//Command to start mongo db server
+
+//Download mongodb from website
+
+//Extract zip file  and go to bin directory.
+//Copy all the files  and put into a folder(mongodb) in local drive.Try to have in in same project folder
+//open command prompt and navigate to that new folder(mongodb)
+//Then run below commands
+//md data
+//C: \Anup\NodeJs\VS2017\Mongodb > mongod - dbpath./data
+//C: \Anup\NodeJs\VS2017\Mongodb > mongod - dbpath./data--rest
+
+//Then to validate the installation Browse below url
+//localhost://27017
+//localhost://28017
